657da5dcf63408250634a9aef7834c0622434c62
EgorSivenko <egor.siv20@gmail.com>
2024-02-10 19:52:49 +0200

    Added `Client` and `Planet` entities

---
+++ src/main/java/org/example/entity/Client.java
@@ -0,0 +1,22 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
